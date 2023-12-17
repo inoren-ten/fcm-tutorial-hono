7792d1cee147e782f854c7fe4c3e8fad89b84dde
HASURO <rentaro170928@icloud.com>
2023-12-17 18:41:03 +0900

    first commit

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDOt002_ebLrUFU500Qa6AFz1sIvtPBX5Q",
+    authDomain: "fcm-tutorial-2f642.firebaseapp.com",
+    projectId: "fcm-tutorial-2f642",
+    storageBucket: "fcm-tutorial-2f642.appspot.com",
+    messagingSenderId: "752648851233",
+    appId: "1:752648851233 :web :7e8bafd311c7ccde497860"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig) ;
